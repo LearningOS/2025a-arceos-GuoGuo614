6c098fec94e4ce703bfc99f751ab18f4e660da60
GuoGuo <2251270969@qq.com>
2025-10-23 11:43:40 +0800

    Finish bump_allocator

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -16,20 +16,46 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator<const SIZE: usize> {}
+pub struct EarlyAllocator<const SIZE: usize> {
+    start: usize,
+    end: usize,
+    b_pos: usize,
+    p_pos: usize,
+}
 
 impl<const SIZE: usize> EarlyAllocator<SIZE> {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start: 0,
+            end: 0 + 64 * 1024 * 1024,
+            b_pos: 0,
+            p_pos: 0 + 64 * 1024 * 1024,
+        }
     }
+}
+
+#[inline]
+fn align_up(addr: usize, align: usize) -> Option<usize> {
+    debug_assert!(align.is_power_of_two());
+    let mask = align - 1;
+    addr.checked_add(mask).map(|x| x & !mask)
+}
+
+fn align_down(addr: usize, align: usize) -> usize {
+    debug_assert!(align.is_power_of_two());
+    addr & !(align - 1)
 }
 
 impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        todo!()
+        self.start = start;
+        let cap = core::cmp::min(size, 64 * 1024 * 1024);
+        self.end = start + cap;
+        self.b_pos = start;
+        self.p_pos = self.end;
     }
 
-    fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
+    fn add_memory(&mut self, _start: usize, _size: usize) -> allocator::AllocResult {
         todo!()
     }
 }
@@ -39,23 +65,43 @@ impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
         &mut self,
         layout: core::alloc::Layout,
     ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
-        todo!()
+        let align = layout.align();
+        let size = layout.size();
+
+        let start = align_up(self.b_pos, align).ok_or(allocator::AllocError::InvalidParam)?;
+        let end = start.checked_add(size).ok_or(allocator::AllocError::InvalidParam)?;
+
+        if end > self.p_pos {
+            return Err(allocator::AllocError::MemoryOverlap);
+        }
+
+        self.b_pos = end;
+        Ok(unsafe {
+            core::ptr::NonNull::new_unchecked(start as *mut u8)
+        })
     }
 
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!()
+        // LIFO free: only rewind if this block is the most recently allocated one.
+        let start = pos.as_ptr() as usize;
+        let size = layout.size();
+        if let Some(end) = start.checked_add(size) {
+            if end == self.b_pos {
+                self.b_pos = start;
+            }
+        }
     }
 
     fn total_bytes(&self) -> usize {
-        todo!()
+        self.p_pos - self.start
     }
 
     fn used_bytes(&self) -> usize {
-        todo!()
+        self.b_pos - self.start
     }
 
     fn available_bytes(&self) -> usize {
-        todo!()
+        self.total_bytes() - self.used_bytes()
     }
 }
 
@@ -67,22 +113,38 @@ impl<const SIZE: usize> PageAllocator for EarlyAllocator<SIZE> {
         num_pages: usize,
         align_pow2: usize,
     ) -> allocator::AllocResult<usize> {
-        todo!()
+        let size = num_pages
+            .checked_mul(Self::PAGE_SIZE)
+            .ok_or(allocator::AllocError::InvalidParam)?;
+        let req_align = if align_pow2 >= usize::BITS as usize {
+            return Err(allocator::AllocError::InvalidParam);
+        } else {
+            core::cmp::max(Self::PAGE_SIZE, 1usize << align_pow2)
+        };
+        let end = align_down(self.p_pos, req_align);
+        let start = end - size;
+
+        if start < self.b_pos {
+            return Err(allocator::AllocError::MemoryOverlap);
+        }
+
+        self.p_pos = start;
+        Ok(start)
     }
 
-    fn dealloc_pages(&mut self, pos: usize, num_pages: usize) {
-        todo!()
+    fn dealloc_pages(&mut self, _pos: usize, _num_pages: usize) {
+        // EarlyAllocator's pages are never freed; this is a no-op by design.
     }
 
     fn total_pages(&self) -> usize {
-        todo!()
+        (self.end - self.b_pos) / Self::PAGE_SIZE
     }
 
     fn used_pages(&self) -> usize {
-        todo!()
+        (self.end - self.p_pos) / Self::PAGE_SIZE
     }
 
     fn available_pages(&self) -> usize {
-        todo!()
+        self.total_pages() - self.used_pages()
     }
 }
